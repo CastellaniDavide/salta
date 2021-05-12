04fae213c34f7fc663efc4ed9246cb7fa85e0647
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-12 22:37:25 +0200

    Auto-update

--- bin/salta.cpp
+++ bin/salta.cpp
@@ -18,25 +18,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//#define DEBUG
+
 // Variabiles
-int N;
+int N, n, next_;
+vector <int> prossimo;
+vector <bool> gia_passato;
 
 // Main code
 int main()
 {
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    // Cncomment the following lines if you want to read/write from files
+#ifndef DEBUG
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif // DEBUG
 
-  // Input
-  cin >> N;
+    // Input
+    cin >> N;
+    prossimo.resize(N + 1);
+    gia_passato.resize(N + 1, false);
+    next_ = 1;
+    n = 0;
 
-  // Code
-  // ...
+    for (int i = 0; i < N; ++i)
+        cin >> prossimo[i + 1];
 
-  // Output
-  cout << N << endl;
+    while (!gia_passato[next_])
+    {
+        gia_passato[next_] = true;
+        next_ = (next_ + prossimo[next_ ]) % N + 1;
+        ++n;
+#ifdef DEBUG
+        cout << "Next: " << next_ << endl;
+#endif // DEBUG
+    }
 
-  // End
-  return 0;
+    // Output
+    cout << N - n << endl;
+    for (int i = 1; i < N + 1; ++i)
+    {
+        if (!gia_passato[i])
+            cout << i << " ";
+    }
+    cout << endl;
+
+    // End
+    return 0;
 }
